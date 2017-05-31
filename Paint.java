be8ef7fc054035f29d693065c947b4beee3bb00e
Byron <byronblocker@gmail.com>
2017-05-31 11:20:39 -0700

    I suck at this

--- Paint.java
+++ Paint.java
@@ -1,5 +1,11 @@
 import java.io.Console;
 
+public class Door {
+  int doorHeight;
+  int doorWidth;
+  int doorArea = doorWidth * doorHeight;
+}
+
 public class Paint {
   public static void main(String[] args) {
     Console console = System.console();
@@ -7,6 +13,8 @@ public class Paint {
     int frontWallWidth;
     int sideWallHeight;
     int sideWallWidth;
+    boolean finishedDoors = false;
+    boolean finishedWindows = false;
 
     System.out.println("What is the height of the front wall?");
     frontWallHeight = Integer.parseInt(console.readLine());
@@ -18,6 +26,16 @@ public class Paint {
     sideWallWidth = Integer. parseInt(console.readLine());
 
     int houseArea = ((frontWallWidth * frontWallHeight) * 2) + ((sideWallWidth * sideWallHeight) * 2);
+
+    while (!finishedDoors){
+      System.out.println("Would you like to enter a new door?");
+      String doorResponse = console.readLine();
+      if {
+
+      } else (doorResponse.equals("no")){
+        finishedDoors = true;
+      }
+    }
 
     System.out.println(houseArea + " sq feet");
 
